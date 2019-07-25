df135a7a99373a40e807079e0e366c2d9a6cf48e
Michael Samorokov <michael.samorokov@gmail.com>
2019-07-24 23:54:33 -0500

    Recorded sound into Downloads folder

--- app/components/App.vue
+++ app/components/App.vue
@@ -7,12 +7,18 @@
                  androidSelectedTabHighlightColor="#ffffff">
             <TabViewItem title="Tab 1">
                 <GridLayout columns="*" rows="*">
-                    <Label class="message" :text="msg" col="0" row="0"/>
+                    <StackLayout col="0" row="0">
+                        <Label class="message" :text="msg"/>
+                        <Button text="Start recording" @tap="onStartRecording"/>
+                        <Button text="Stop recording" @tap="onStopRecording"/>
+                    </StackLayout>
                 </GridLayout>
             </TabViewItem>
             <TabViewItem title="Tab 2">
                 <GridLayout columns="*" rows="*">
-                    <Label class="message" text="Tab 2 Content" col="0" row="0"/>
+                    <StackLayout col="0" row="0">
+                        <Button text="Write file" @tap="onWriteFile"/>
+                    </StackLayout>
                 </GridLayout>
             </TabViewItem>
             <TabViewItem title="Tab 3">
@@ -25,14 +31,35 @@
 </template>
 
 <script lang="ts">
-  import { TNSPlayer, TNSRecorder } from 'nativescript-audio';
+    import { TNSPlayer, TNSRecorder, AudioRecorderOptions } from 'nativescript-audio';
+    import { error } from 'util';
+    import { knownFolders, Folder, File, path } from "tns-core-modules/file-system";
+    import { isAndroid } from "tns-core-modules/platform";
+    import { alert } from "tns-core-modules/ui/dialogs";
+
+  declare var android;
+
 
   export default {
     data() {
       let message = "Hello";
+      //const temp: Folder = <Folder>knownFolders.temp();
+      //const folder: Folder = <Folder>temp.getFolder("test-recs");
+
+      let audioFolder = knownFolders.currentApp().getFolder("audio-test").path;
+      const fileName = "/test-record.mp3";
+
+      if (isAndroid) {
+        const permissions = require("nativescript-permissions");
+        permissions.requestPermission(android.Manifest.permission.WRITE_EXTERNAL_STORAGE, "I need these permissions because I'm cool")
+            .then(() => {
+                audioFolder = path.join(android.os.Environment.getExternalStoragePublicDirectory(android.os.Environment.DIRECTORY_DOWNLOADS).getAbsolutePath(), fileName);
+            });
+    }
+
       const recorder = new TNSRecorder();
       recorder.debug = true;
-      if(!recorder.hasRecordPermission()){
+      if(!TNSRecorder.CAN_RECORD()){
           message = "Asking for MIC permisison";
           recorder.requestRecordPermission();
       } else{
@@ -40,7 +67,40 @@
       }
 
       return {
-        msg: message
+        msg: message,
+        onStartRecording: async () => {
+            await recorder.start({
+                filename: path.normalize(audioFolder),   
+            infoCallback: function(recorder: any, info: number, extra: number) {
+                console.log(`infoCallback: ${info} => ${extra}`);
+            },
+            errorCallback: function(recorder: any, error: number, extra: number) {
+                console.log('errorCallback');
+                alert('Error recording.');
+            }
+            });
+        },
+        onStopRecording: async () => {
+            await recorder.stop();
+            const imageFile = File.fromPath(path.normalize(audioFolder));
+            const binarySource = imageFile.readSync((err) => {
+                console.log(err);
+            });
+            console.log(binarySource);
+        },
+        onWriteFile: async () => {
+            const fileToWrite = path.normalize(knownFolders.currentApp() + "test-file.txt");
+            const file = File.fromPath(fileToWrite);
+
+            try{
+                await file.writeText("foo bar");
+
+                let text = await file.readText();
+                console.log(text);
+            } catch (error){
+                console.log(error);
+            }
+        }
       }
     }
   }
